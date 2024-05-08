68e8b5a81b65a39ebf32a87cc3e2bc108ee803d6
ujii <wcm9763@naver.com>
2024-05-08 11:04:51 +0900

    ✨ SimpleTestController 작성
    RequsetMapping과 GetMapping 어노테이션

---
+++ src/main/java/com/homepage/likelion/test/SimpleTestController.java
@@ -0,0 +1,23 @@
+package com.homepage.likelion.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/simpleText")
+public class SimpleTestController {
+
+    // GET http://localhost:8080/api/simpleText/success
+    // 위 경로로 요청이 들어오면 "안녕"이라는 문장을 리턴하는 api
+    @GetMapping("/success")
+    public String simpleTextSuccess() {
+        return "안녕";
+    }
+
+    // GET http://localhost:8080/api/simpleText/fail
+    @GetMapping("/fail")
+    public String simpleTextFail() {
+        return "안녕";
+    }
+}
